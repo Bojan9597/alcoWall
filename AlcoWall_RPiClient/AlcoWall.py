9293ea99b9170c76c03b1874932fa7f79eae2a00
Bojan <bojanpejic97@gmail.com>
2024-09-12 17:02:59 +0200

    update script test4

--- AlcoWall_RPiClient/AlcoWall.py
+++ AlcoWall_RPiClient/AlcoWall.py
@@ -5,7 +5,7 @@ from PySide6.QtMultimediaWidgets import QVideoWidget
 from PySide6.QtMultimedia import QMediaPlayer
 from PySide6.QtGui import QPixmap
 from VideoWidget import VideoWidget
-#checking update script  ssss
+#checking update script  sssss
 class AlcoWall(QWidget):
     _instance = None
 
