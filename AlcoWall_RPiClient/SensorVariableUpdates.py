cb04b79df32c791a35d54335475e434f943130c8
Bojan <bojanpejic97@gmail.com>
2024-09-20 19:19:40 +0200

    Coins successufuly added to database

--- AlcoWall_RPiClient/SensorVariableUpdates.py
+++ AlcoWall_RPiClient/SensorVariableUpdates.py
@@ -5,12 +5,14 @@ import requests
 from datetime import datetime
 from PySide6.QtCore import QTimer
 from AlcoWall import AlcoWall
-from CONSTANTS import TARGET_PLATFORM_ARCHITECTURE, TARGET_PLATFORM_SYSTEM
+import os
+from CONSTANTS import TARGET_PLATFORM_ARCHITECTURE, TARGET_PLATFORM_SYSTEM, DEVICE_ID
 
 alcoWall = AlcoWall()
 
 class SensorVariableUpdates:
     def __init__(self):
+        self.counter = 0
         # self.coin_check_timer = QTimer()
         # self.coin_check_timer.timeout.connect(self.check_variable_updates)
         # self.coin_check_timer.start(1000)
@@ -44,13 +46,13 @@ class SensorVariableUpdates:
         """Separate thread for checking sensor updates and sending data."""
         while True:
             self.check_variable_updates()
-            threading.Event().wait(0.2)
+            threading.Event().wait(1)
 
     def check_variable_updates(self):
         # Handling coin acceptor
         if self.coinAcceptor:
             if self.coinAcceptor.get_credit() > 0:
-                self.record_coin_insertion(self.coinAcceptor.get_credit())
+                self.record_coin_insertion(self.coinAcceptor.get_credit()) 
                 alcoWall.update_credit(self.coinAcceptor.get_credit())
                 self.coinAcceptor.set_credit(0)
         else:
@@ -58,8 +60,7 @@ class SensorVariableUpdates:
                 with open("testFiles/coinInserted.txt", "r") as file:
                     content = file.read().strip()
                 if content != "" and content != "0":
-                    self.record_coin_insertion(float(content))
-                    self.send_coin_insertions()
+                    self.send_coin_insertions(float(content))
                     alcoWall.update_credit(float(content))
             except FileNotFoundError:
                 pass
@@ -96,31 +97,79 @@ class SensorVariableUpdates:
                 alcoWall.coin_stuck = error_data["coin_stuck"]
         except FileNotFoundError:
             pass
-
-    def record_coin_insertion(self, credit):
-        """Record a coin insertion with the current timestamp."""
-        self.coin_insertions.append({
+    
+    def write_credit_to_json_file(self, credit):
+        """
+        @brief Writes the results of the alcohol check to a JSON file.
+        """
+        data = {
             "device_id": 1,  # Replace with actual device ID if needed
             "cash_value": credit,
             "date": datetime.now().isoformat()  # Use current timestamp
-        })
+        }
+        # Ensure the directory exists
+        self.ensure_directory_exists("jsonFiles")
 
-    def send_coin_insertions(self):
+        try:
+            with open("jsonFiles/coinInserted.json", "a") as json_file:
+                json.dump(data, json_file)
+                json_file.write("\n")  # Write each entry on a new line
+        except IOError as e:
+            print(f"An error occurred while writing to the JSON file: {e}")
+    
+    def ensure_directory_exists(self, directory):
+        """
+        Ensure the given directory exists. If it does not exist, create it.
+        """
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    def send_coin_insertions(self, credit):
         """Try sending the coin insertions to the server."""
-        if not self.coin_insertions:
-            return  # No coin insertions to send
-        print(f"Sending coin insertions: {self.coin_insertions}")
+        self.coin_insertions.clear()
+        self.write_credit_to_json_file(float(credit))
 
+        with open("jsonFiles/coinInserted.json", "r") as json_file:
+            for line in json_file:
+                data = json.loads(line)
+                # Store device_id, alcohol_level, and timestamp
+                self.coin_insertions.append({
+                    "device_id": DEVICE_ID,
+                    "cash_value": credit,
+                    "date": data["date"]
+                })
+        
+        self.send_coin_insertions_to_database(self.coin_insertions)
+                
+        if os.path.exists("jsonFiles/coinInserted.json"):
+            with open("jsonFiles/coinInserted.json", "w") as json_file:
+                for credit in self.coin_insertions:
+                    json.dump(credit, json_file)
+                    json_file.write("\n")
+        
+
+    def send_coin_insertions_to_database(self, credit):
+        success = False
         try:
             response = requests.post("https://node.alkowall.indigoingenium.ba/cash/add_cash_multiple", json=self.coin_insertions)
             if response.status_code == 200:
                 response_data = response.json()
                 if response_data.get("message") == "Cash status updated successfully for multiple devices.":
+                    success = True
+                    print("Coin insertions sent successfully.")
+                    print(self.coin_insertions)
                     self.coin_insertions.clear()  # Clear the list after successful sending
+
                 else:
                     print(f"Unexpected response from the server: {response_data}")
+                    success = False
             else:
                 print(f"Failed to send coin insertions. Status code: {response.status_code}")
+                success = False
         except requests.RequestException as e:
-            pass
+            success = False
+            return success
+        print("Coin insertions sent successfully.")
+        return success
+        
             # print(f"Error while sending coin insertions: {e}")
