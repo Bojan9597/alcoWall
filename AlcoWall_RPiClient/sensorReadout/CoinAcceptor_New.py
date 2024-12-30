4ef3c716cd3995fbe5599c8cff45bd9f9b421366
Bojan <bojanpejic97@gmail.com>
2024-12-30 02:00:40 +0100

    stupid

--- AlcoWall_RPiClient/sensorReadout/CoinAcceptor_New.py
+++ AlcoWall_RPiClient/sensorReadout/CoinAcceptor_New.py
@@ -328,7 +328,7 @@ import time
 import threading
 from PySide6.QtCore import Signal
 class CoinAcceptor:
-    CoinAcceptedSignal = Signal(int)Components/AlcoWall.py
+    CoinAcceptedSignal = Signal(int)
     def __init__(self):
         port = find_coin_acceptor()
         coin_validator_connection = make_serial_object(port)
